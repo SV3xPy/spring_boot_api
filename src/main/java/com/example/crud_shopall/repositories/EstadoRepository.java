23dfc904dbab48cf5100cfa193f4fe144ee8ce83
Miguel Ruiz <miguel_an85@hotmail.com>
2023-10-12 08:40:56 -0600

    Este commit es para la creación de la carpeta controllers, repositories, services y todo el modelo de Estado

---
+++ src/main/java/com/example/crud_shopall/repositories/EstadoRepository.java
@@ -0,0 +1,7 @@
+package com.example.crud_shopall.repositories;
+
+import com.example.crud_shopall.model.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepository extends JpaRepository<Estado,Long> {
+}
