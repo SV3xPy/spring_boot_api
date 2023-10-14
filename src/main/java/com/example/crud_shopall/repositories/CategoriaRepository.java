6a33868f97ca2b5d2028af671a95c769e66fbbca
CrisAOH <20031091@itcelaya.edu.mx>
2023-10-14 02:04:06 -0600

    Se han terminado los repositorios para las tablas Categoria, Marca, Producto y ProductoCategoria.

---
+++ src/main/java/com/example/crud_shopall/repositories/CategoriaRepository.java
@@ -0,0 +1,6 @@
+package com.example.crud_shopall.repositories;
+import com.example.crud_shopall.model.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+}
---
+++ src/main/java/com/example/crud_shopall/repositories/ProductoCategoriaRepository.java
@@ -0,0 +1,6 @@
+package com.example.crud_shopall.repositories;
+import com.example.crud_shopall.model.ProductoCategoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductoCategoriaRepository extends JpaRepository<ProductoCategoria, Long>{
+}
