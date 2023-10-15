38ace78a0c0e5001eba7f991894540bd9ba9c4c8
Miguel Ruiz <miguel_an85@hotmail.com>
2023-10-14 22:35:38 -0600

    CRUDs de Lada, Telefono y Sexo listos, ya también tienen sus relaciones. NOTA: Para relacionar Sexo con Cliente y Empleado, únicamente necesita de los modelos de Cliente y Empleado, pero como yo no los tengo, comenté en el modelo de Sexo la sección en donde se relaciona al Sexo con Cliente y Empleado.

--- src/main/java/com/example/crud_shopall/services/SexoService.java
+++ src/main/java/com/example/crud_shopall/services/SexoService.java
@@ -4,12 +4,17 @@ import com.example.crud_shopall.model.Rol;
 import com.example.crud_shopall.model.Sexo;
 import com.example.crud_shopall.repositories.SexoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class SexoService {
+    HashMap<String,Object> datos;
     private final SexoRepository sexoRepository;
 
     @Autowired
@@ -19,5 +24,59 @@ public class SexoService {
 
     public List<Sexo> getSexo(){
         return this.sexoRepository.findAll();
+    }
+
+    public ResponseEntity<Object> newSexo(Sexo sexo) {
+        datos = new HashMap<>();
+        sexoRepository.save(sexo);
+        datos.put("data",sexo);
+        datos.put("message","Registro insertado con Éxito");
+        return new ResponseEntity<>(
+                sexo,
+                HttpStatus.CREATED
+        );
+    }
+
+    public ResponseEntity<Object> deleteSexo(Long id) {
+        datos = new HashMap<>();
+        boolean exists=this.sexoRepository.existsById(id);
+        if(!exists){
+            datos.put("error",true);
+            datos.put("message","No existe un sexo con ese ID");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }
+        sexoRepository.deleteById(id);
+        datos.put("message","Sexo con ID: "+id+" eliminado con exito.");
+        return new ResponseEntity<>(
+                datos,
+                HttpStatus.ACCEPTED
+        );
+
+    }
+
+    public ResponseEntity<Object> updateSexo(Sexo sexo, Long id) {
+        datos = new HashMap<>();
+        Optional<Sexo> sexoOpcional=this.sexoRepository.findById(id);
+        if(sexoOpcional.isEmpty()){
+            datos.put("error",true);
+            datos.put("message","No existe un sexo con ese ID");
+            return new ResponseEntity<>(
+                    datos,
+                    HttpStatus.CONFLICT
+            );
+        }
+        Sexo oldSexo = sexoOpcional.get();
+        //SE ACTUALIZA EL NOMBRE
+        oldSexo.setSexo(sexo.getSexo());
+        sexoRepository.save(oldSexo);
+        datos.put("message","Sexo con ID: "+id+" actualizado con exito.");
+        datos.put("data",oldSexo);
+        return new ResponseEntity<>(
+                datos,
+                HttpStatus.ACCEPTED
+        );
     }
 }
