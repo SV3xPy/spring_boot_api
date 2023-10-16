70454cf005f733b8937bc92cc4bff1f0be07a139
miguelesc1409 <127141525+miguelesc1409@users.noreply.github.com>
2023-10-16 12:35:00 -0600

    Main V.2.
    Main con la solucion del error

--- src/main/java/com/example/crud_shopall/CrudShopallApplication.java
+++ src/main/java/com/example/crud_shopall/CrudShopallApplication.java
@@ -1,9 +1,17 @@
 package com.example.crud_shopall;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class},scanBasePackages={
+		"com.example.crud_shopall.repositories"})
+@RestController
+
 public class CrudShopallApplication {
 
 	public static void main(String[] args) {
